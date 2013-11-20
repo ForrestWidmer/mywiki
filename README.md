8afe24a5bf90b44ad7e0bdef7ba3b84409b21d17
Forrest Widmer <fwid77@gmail.com>
2013-11-20 13:45:57 -0700

    First commit and README update for My Wiki.

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Wiki
+
+My Wiki is an application that allows users to create public and private Markdown-based wikis. Accounts are free to create public wikis with the option of upgrading to a paid premium account. Premium accounts have the ability to make their wikis private and to add an unlimited amount of collaboraters. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mywiki::Application.config.secret_token = '5d18f8be2396e413fcf651f47678d2b6450267660efa2ece91ee834dc0c7dffb75c67f492cb36105230ddc4feef4f1e9d2b6192b65de7e850a183a337dc7bb78'
