965661300f59b73725ab046102ea553a9c1b6705
ForrestWidmer <forrest.widmer@gmail.com>
2014-04-17 16:22:56 -0700

    Updated image uploader to size down uploaded images.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-  process :resize_to_fill => [300, 300]
+  # process :resize_to_fill => [300, 300]
 
   # Create different versions of your uploaded files:
 
@@ -40,7 +40,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :tab do
-    process :resize_to_fill => [150, 150]
+    process :resize_to_limit => [240, 180]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
